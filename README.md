c76b22c147da7212f61298fd40fd0892b7405a29
Diego Ferreira <diferreira.program@gmail.com>
2022-09-16 16:57:56 -0300

    Topbar

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# TO-DO-MANAGER
+# To Do App Manager
+
+### App for study design mobile in Delphi FMX
+App for management To Do list.
--- VIEW/Task.App.View.Main.pas
+++ VIEW/Task.App.View.Main.pas
@@ -4,10 +4,23 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts, FMX.Objects, Skia.FMX,
+  FMX.Effects;
 
 type
   TTaskAppViewMain = class(TForm)
+    Profile: TRoundRect;
+    LblAppName: TSkLabel;
+    Menu: TRectangle;
+    BtnMenuIcon: TSkSvg;
+    ShadowEffect1: TShadowEffect;
+    ToolBar: TToolBar;
+    BtnSearch: TSkSvg;
+    Border: TPie;
+    ContainerProfileImage: TRectangle;
+    ProfileImage: TCircle;
+    Avatar: TSkSvg;
   private
     { Private declarations }
   public
